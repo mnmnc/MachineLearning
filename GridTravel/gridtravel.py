28eacb981a2d3f04cb37a8b3bfdcd76570752f47
mnmnc <chooseright+git@gmail.com>
2014-11-13 14:49:09 +0100

    -

--- GridTravel/gridtravel.py
+++ GridTravel/gridtravel.py
@@ -1,4 +1,25 @@
 
+# K---P---T---X---Z
+# |   |   |   |   |
+# G---L---Q---U---Y
+# |   |   |   |   |
+# D---H---M---R---W
+# |   |   |   |   |
+# B---E---I---N---S
+# |   |   |   |   |
+# A---C---F---J---O
+
+# K-3-P-3-T-3-X-2-Z
+# |25 |10 |4  |3  |2
+# G-1-L-2-Q-2-U-2-Y
+# |20 |8  |3  |2  |3
+# D-2-H-3-M-2-R-1-W
+# |10 |4  |2  |3  |2
+# B-3-E-2-I-2-N-2-S
+# |6  |4  |3  |3  |2
+# A-4-C-1-F-2-J-3-O
+
+
 graph = {
 	'A' : [ {'B':6},	{'C':4} ],
 	'K' : [ {'G':25},	{'P':3} ],
@@ -31,9 +52,43 @@ def set_initial_node(node):
 	global initial_node
 	initial_node = node
 
+
 def set_destination_node(node):
 	global destination_node
 	destination_node = node
 
 
+def fill_unvisited_verticies():
+	global unvisited
+	for key in graph:
+		print("Adding", key, "to unvisited")
+		unvisited.append(key)
 
+
+def fill_distance_matrix():
+	global distance_matrix
+	for key in graph:
+		print("Adding initial distance 99999 for", key)
+		distance_matrix.update({key:99999})
+
+
+def calculate_distance_to_neighbours():
+	global initial_node, distance_matrix
+	neighbours = graph[initial_node]
+	for neighbour in neighbours:
+		weight = neighbours[neighbour]
+		if distance_matrix[initial_node] + weight < distance_matrix[neighbour]:
+			distance_matrix[neighbour] = distance_matrix[initial_node] + weight
+
+def get_node_with_smalles_distance():
+	global distance_matrix, unvisited
+	smallest_distance_node = None
+	for node in unvisited:
+		if smallest_distance_node == None:
+			smallest_distance_node = node
+		else:
+			if distance_matrix[smallest_distance_node] > distance_matrix[node]:
+				smallest_distance_node = node
+
+def process_nodes():
+	pass
