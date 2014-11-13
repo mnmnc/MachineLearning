39d4e3eebefe6bc5b7e9955d55c65b481f83c6b9
mnmnc <chooseright+git@gmail.com>
2014-11-13 17:02:35 +0100

    Working example. Lots of redundant debuging info

--- GridTravel/gridtravel.py
+++ GridTravel/gridtravel.py
@@ -19,6 +19,7 @@
 # |6  |4  |3  |3  |2
 # A-4-C-1-F-2-J-3-O
 
+import pprint
 
 graph = {
 	'A' : [ {'B':6},	{'C':4} ],
@@ -58,8 +59,9 @@ def set_destination_node(node):
 	destination_node = node
 
 
-def fill_unvisited_verticies():
+def fill_unvisited_vertices():
 	global unvisited
+	unvisited = []
 	for key in graph:
 		print("Adding", key, "to unvisited")
 		unvisited.append(key)
@@ -67,28 +69,92 @@ def fill_unvisited_verticies():
 
 def fill_distance_matrix():
 	global distance_matrix
+	distance_matrix = {}
 	for key in graph:
-		print("Adding initial distance 99999 for", key)
-		distance_matrix.update({key:99999})
+		if key == "A":
+			print("Adding initial distance 99999 for", key)
+			distance_matrix.update({key:0})
+		else:
+			print("Adding initial distance 99999 for", key)
+			distance_matrix.update({key:99999})
 
 
 def calculate_distance_to_neighbours():
 	global initial_node, distance_matrix
 	neighbours = graph[initial_node]
+	print("Iterating over neighbours")
 	for neighbour in neighbours:
-		weight = neighbours[neighbour]
-		if distance_matrix[initial_node] + weight < distance_matrix[neighbour]:
-			distance_matrix[neighbour] = distance_matrix[initial_node] + weight
+		print("\tProcessing neighbour:", neighbour)
+		for neighbour_name in neighbour:
+			print("\t\tneighbour name:", neighbour_name)
+			weight = neighbour[neighbour_name]
+			print("\t\tneighbour weight:", weight)
+			print("\t\t\tComparing:", str(distance_matrix[initial_node] + weight), "and", str(distance_matrix[neighbour_name])  )
+			if distance_matrix[initial_node] + weight < distance_matrix[neighbour_name]:
+				print("\t\t\tSMALLER!")
+				distance_matrix[neighbour_name] = distance_matrix[initial_node] + weight
 
-def get_node_with_smalles_distance():
+
+def get_node_with_smallest_distance():
 	global distance_matrix, unvisited
+	smallest_value = 99999
 	smallest_distance_node = None
 	for node in unvisited:
-		if smallest_distance_node == None:
+		if distance_matrix[node] < smallest_value:
+			smallest_value = distance_matrix[node]
 			smallest_distance_node = node
-		else:
-			if distance_matrix[smallest_distance_node] > distance_matrix[node]:
-				smallest_distance_node = node
+	if smallest_value == 99999:
+		return "A"
+	else:
+		return smallest_distance_node
+
+def get_closer_neighbour(node):
+	print("Searching for closest neighbour of :", node)
+	smallest_value = 99999
+	closest_neighbour = None
+	for neighbours in graph[node]:
+		print("\tNeighbours:", neighbours)
+		for neighbour_name in neighbours:
+			print("\t\tNeighbour:", neighbour_name, " Nvalue:", distance_matrix[neighbour_name], " smallest", smallest_value  )
+			if distance_matrix[neighbour_name] < smallest_value:
+				print("\t\t\tnew smallest value", distance_matrix[neighbour_name], " node", neighbour_name )
+				smallest_value = distance_matrix[neighbour_name]
+				closest_neighbour = neighbour_name
+	return closest_neighbour
+
+def find_reverse_path():
+	global destination_node, initial_node
+	print("Shortest path from Z to A:")
+	current_node = destination_node
+	for node in graph:
+		print(current_node)
+		if current_node == "A":
+			break
+		current_node = get_closer_neighbour(current_node)
+
+
+
 
 def process_nodes():
 	pass
+
+def main():
+
+	printer = pprint.PrettyPrinter(width=50)
+	global initial_node, distance_matrix, unvisited, destination_node
+	fill_unvisited_vertices()
+	fill_distance_matrix()
+	set_destination_node("Z")
+
+	for i in range(len(graph)):
+		set_initial_node(get_node_with_smallest_distance())
+		print(initial_node)
+		calculate_distance_to_neighbours()
+		printer.pprint(distance_matrix)
+		unvisited.remove(initial_node)
+		printer.pprint(unvisited)
+
+	find_reverse_path()
+
+if __name__ == "__main__":
+	main()
