de1b4f782c175b2e5e74dd3e61089ffbcc900bd3
mnmnc <mnmnc@mail.com>
2014-11-28 00:04:55 +0100

    Cleaning

--- STSP_2D/tsales/tsales/tsales/Source.cpp
+++ STSP_2D/tsales/tsales/tsales/Source.cpp
@@ -29,7 +29,6 @@ int DEBUG = 0;
 using namespace std;
 
 // PRINTING FUNCTIONS
-	void print_vector(vector<string> vec);
 	void print_path(vector<string> path);
 	void print_path_and_values(vector<string> path, map<string, vector<int> > mapa);
 	void print_array_of_paths(vector<vector<string>> path_array);
@@ -41,7 +40,6 @@ using namespace std;
 	double get_path_length(vector<string> nodes, map<string, vector<int> > mapa);
 
 // COMPARE FUNCTIONS
-	bool compare_two(vector<string> v1, vector<string> v2);
 	bool contains_cycles(vector<string> path);
 
 // COLLECTION FUNCTIONS
@@ -67,30 +65,26 @@ int main(){
 	unsigned int iteration_threshold = 100;
 	unsigned int worst_possible_path_lenght = UINT_MAX;
 
+	// SEEDING
 	srand(time(NULL));
 	
-
 	// BUILDING DATA MAP
 	map<string, vector<int> > mapa = build_data_map();
 	population_size = mapa.size();
 
-
-
 	// CREATING INITIAL POPULATION
-	clock_t begin = clock();
 	vector<vector<string>> population_1 = create_initial_population(mapa, population_size);
-	clock_t end = clock();
-	double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
-	cout << "Time spent on creating initial population: " << elapsed_secs << endl; 
 
-	solve(population_1, mapa, 100);
+	// SOLVING
+	clock_t begin_pt = clock();
+	solve(population_1, mapa, iteration_threshold);
+	//clock_t end_pt = clock();
+	std::cout << "Time spent solving " << double(clock() - begin_pt) / CLOCKS_PER_SEC << endl;
 
 	system("PAUSE");
 	return 0;
 	
 }
-
-
 
 void solve(vector<vector<string>> population_1, map<string, vector<int> > mapa, int iterations){
 
@@ -98,104 +92,108 @@ void solve(vector<vector<string>> population_1, map<string, vector<int> > mapa, 
 	vector<string> best_path;
 	unsigned int best_path_reached_in_iteration = 0;
 	unsigned int iteration_counter = 0;
+	unsigned int total_iterations = iterations;
 	vector<vector<string>> local_population = population_1;
 
+	// GENERATE GIVEN NUMBER OF GENERATIONS
 	while ( iterations > 0 ){
-		clock_t begin_iteration = clock();
 		vector<vector<string>> next_population;
-		clock_t begin_better_half = clock();
 		vector<vector<string>> better_half = pick_better_half(local_population, mapa, UINT_MAX);
-		clock_t end_better_half = clock();
-		//cout << "Time spent on picking better half " << double(end_better_half - begin_better_half) / CLOCKS_PER_SEC << endl;
 
+		// FILL WHOLE POPULATION
+		//while (next_population.size() < (population_1.size() /1.5)){
 		while (next_population.size() < population_1.size()){
 
-			clock_t begin_pt = clock();
+			// PICKING PARENTS
 			vector<vector<string>> parents = pick_two(better_half);
-			clock_t end_pt = clock();
-			//cout << "Time spent on picking parents " << double(end_pt - begin_pt) / CLOCKS_PER_SEC << endl;
 
-			if (DEBUG == 1) cout << "\nPARENTS:" << endl;
-			if (DEBUG == 1) print_array_of_paths(parents);
-
+			// CROSSING CHILDREN
 			vector<vector<string>> children = crossover(parents, mapa);
+
+			// MUTATING CHILDREN
 			children = mutation(children, mapa);
 
+			// VERIFYING CHILDREN VALIDITY
 			bool cycles_in_daughter = contains_cycles(children.at(0));
 			bool cycles_in_son = contains_cycles(children.at(1));
-			bool check_identical = compare_two(children.at(0), children.at(1));
 
-			if ( check_identical == false ){
+			// IF NOT TWINS
+			if (children.at(0) != children.at(1)){
 				if ( next_population.size() < 1 ){
+					// IF POPULATION IS FRESH - EXTEND IT WITHOUT CHECKING
 					if (!cycles_in_daughter) next_population.push_back(children.at(0));
 					if (!cycles_in_son) next_population.push_back(children.at(1));
 				}
 				else {
+					// CHECK FOR POTENTIAL GENE DUPLICATION
 					int daughter_check = 0;
 					int son_check = 0;
 					for (unsigned int i = 0;  i < next_population.size(); ++i){
 						if (children.at(0) == next_population.at(i)) ++daughter_check;
 						if (children.at(1) == next_population.at(i)) ++son_check;
 					}
+					// ADD THOSE THAT WILL BE UNIQUE IN POPULATION
 					if (daughter_check < 1 && !cycles_in_daughter) next_population.push_back(children.at(0));
 					if (son_check < 1 && !cycles_in_son) next_population.push_back(children.at(1));
 				}
-			} 
+			}
+			// IF TWINS - CHOOSE ONE
+			else {
+				// CHECK FOR POTENTIAL GENE DUPLICATION
+				int daughter_check = 0;
+				for (unsigned int i = 0; i < next_population.size(); ++i){
+					if (children.at(0) == next_population.at(i)){
+						++daughter_check;
+					}
+				}
+				if (daughter_check < 1 && !cycles_in_daughter) next_population.push_back(children.at(0));
+			}
 		}
-		clock_t end_iteration = clock();
-		double iteration_elapsed_secs = double(end_iteration - begin_iteration) / CLOCKS_PER_SEC;
-		cout << "Time spent on iteration " << iteration_elapsed_secs << endl; 
 
-		// PICK BEST PATH
-		vector<string> local_best_path =  find_best_path(next_population, mapa, UINT_MAX);
+		// PICK BEST PATH AND GET LENGTH
+		vector<string> local_best_path = find_best_path(next_population, mapa, UINT_MAX);
 		double local_best_path_length = get_path_length(local_best_path, mapa);
-		cout << "Iteration " << iteration_counter+1 << ". Best path length: " << local_best_path_length << endl;
+
+		// INFORM ABOUT CURRENT ITERATION
+		cout << "[INFO] Iteration " << iteration_counter+1 << ". Best path length: " << local_best_path_length << endl;
+
+		// CHECK IF CURRENT ONE IS BEST
 		if (local_best_path_length < best_path_length) {
 			best_path_length = local_best_path_length;
 			best_path = local_best_path;
+			best_path_reached_in_iteration = total_iterations - iterations;
 		}
+		// T = T+1
 		local_population = next_population;
 		++iteration_counter;
 		--iterations;
 	}
-	cout << "Best path length: " << best_path_length << " reached in " << iteration_counter << " iteration." << endl;
+	cout << "[FINAL] Best path length: " << best_path_length << " reached in " << best_path_reached_in_iteration << " iteration." << endl;
 
 }
 
-
-
 vector<vector<string>> mutation(vector<vector<string>> children, map<string, vector<int> > mapa){
-	if (DEBUG == 1) cout << "In mutation" << endl;
-	if ( get_random_num(1000) > 21 ){
-	//if ( get_random_num(20) > 21 ){
-		if (DEBUG == 1) cout << "No mutation" << endl;
+
+	// MUTATION HAPPENS FOR 0.02 PERCENTILE
+	if (get_random_num(1000) > 21){
 		return children;
 	} 
 
-	if (DEBUG == 1) print_array_of_paths(children);
+	// VARIABLES
 	vector<vector<string>> mutated_children;
-
-	if (DEBUG == 1) cout << "1" << endl;
 	vector<string> daughter = children.at(0);
-	if (DEBUG == 1) cout << "2" << endl;
 	vector<string> son = children.at(1);
-	if (DEBUG == 1) cout << "3" << endl;
 
-	if (DEBUG == 1) cout << "Searching for mutation points" << endl;
 	// CHOOSE MUTATION POINTS
 	int mutation_point_1 = get_random_num( (children.at(0)).size() );
 	int mutation_point_2 = get_random_num( (children.at(0)).size() );
-	while ( mutation_point_1 == mutation_point_2){
+	while (mutation_point_1 == mutation_point_2){
 		mutation_point_2 = get_random_num( (children.at(0)).size() );
 	}
-
-	if (DEBUG == 1) cout << "Mutation " << mutation_point_1 << " Mutation " << mutation_point_2 << endl;
 
 	// GET CROSSING POINTS IN ORDER
 	unsigned int smaller = (mutation_point_1 < mutation_point_2)?(mutation_point_1):(mutation_point_2);
 	unsigned int bigger = (mutation_point_1 < mutation_point_2)?(mutation_point_2):(mutation_point_1);
-
-	if (DEBUG == 1) cout << smaller << " < " << bigger << endl;
 
 	// COLLECTING SUBPATH FOR MUTATION
 	vector<string> daughter_mutation;
@@ -204,14 +202,10 @@ vector<vector<string>> mutation(vector<vector<string>> children, map<string, vec
 		daughter_mutation.push_back(daughter.at(i));
 		son_mutation.push_back(son.at(i));
 	}
-	if (DEBUG == 1) cout << "Before reversal:" << endl;
-	if (DEBUG == 1) print_path(daughter_mutation);
-	if (DEBUG == 1) print_path(son_mutation);
+
+	// REORDERING SUBGENOMS
 	reverse(daughter_mutation.begin(), daughter_mutation.end());
 	reverse(son_mutation.begin(), son_mutation.end());
-	if (DEBUG == 1) cout << "After reversal:" << endl;
-	if (DEBUG == 1) print_path(daughter_mutation);
-	if (DEBUG == 1) print_path(son_mutation);
 
 	// MUTATING
 	unsigned int index = 0;
@@ -221,10 +215,7 @@ vector<vector<string>> mutation(vector<vector<string>> children, map<string, vec
 		++index;
 	}
 
-	if (DEBUG == 1) cout << "After mutation:" << endl;
-	if (DEBUG == 1) print_path(daughter);
-	if (DEBUG == 1) print_path(son);
-
+	// FILLING THE RESULT
 	mutated_children.push_back(daughter);
 	mutated_children.push_back(son);
 
@@ -239,7 +230,6 @@ vector<vector<string>> crossover(vector<vector<string>> parents, map<string, vec
 	// CHECK WHETHER PARENTS LIKE EACH OTHER ENOUGH
 	if ( get_random_num(100) > 70 ) return parents;
 
-	if (DEBUG == 1) cout << "Attempting crossing" << endl;
 	// CHOOSE CROSSING POINTS
 	int crossing_point_1 = get_random_num( (parents.at(0)).size() );
 	int crossing_point_2 = get_random_num( (parents.at(0)).size() );
@@ -247,29 +237,18 @@ vector<vector<string>> crossover(vector<vector<string>> parents, map<string, vec
 		crossing_point_2 = get_random_num( (parents.at(0)).size() );
 	}
 
-	if (DEBUG == 1) cout << "Cross " << crossing_point_1 << " Cross " << crossing_point_2 << endl;
 	// GET CROSSING POINTS IN ORDER
-	int smaller = (crossing_point_1 < crossing_point_2)?(crossing_point_1):(crossing_point_2);
-	int bigger = (crossing_point_1 < crossing_point_2)?(crossing_point_2):(crossing_point_1);
-
-	if (DEBUG == 1) cout << smaller << " < " << bigger << endl;
+	unsigned int smaller = (crossing_point_1 < crossing_point_2)?(crossing_point_1):(crossing_point_2);
+	unsigned int bigger = (crossing_point_1 < crossing_point_2)?(crossing_point_2):(crossing_point_1);
 
 	vector<string> daughter;
 	vector<string> son;
 
-	if (DEBUG == 1) cout << "Mothers gene size: " << parents.at(0).size() << endl;
 	// FILLING CHILDREN WITH EMPTY GENES
 	for (unsigned int i = 0; i < (parents.at(0)).size(); ++i ){
-		if (DEBUG == 1) cout << "Filling children " << i << endl; 
 		daughter.push_back(" ");
 		son.push_back(" ");
 	}
-	if (DEBUG == 1) cout << "After zero filling:" << endl;
-
-	if (DEBUG == 1) print_path(daughter);
-	if (DEBUG == 1) print_path(son);
-
-	if (DEBUG == 1) cout << "After cross filling: " << endl;
 
 	// FILLING CHILDREN WITH PARENT GENES BETWEEN CROSSING POINTS
 	// SON GETS GENES FROM MOTHER, DAUGHTER FROM FATHER
@@ -283,8 +262,6 @@ vector<vector<string>> crossover(vector<vector<string>> parents, map<string, vec
 		son[i] = (parents.at(0)).at(i);
 		father_data.push_back((parents.at(0)).at(i));
 	}
-	if (DEBUG == 1)print_path(daughter);
-	if (DEBUG == 1)print_path(son);
 
 	// CREATE RULES FROM PARENTS
 	vector<vector<string>> rules;
@@ -295,12 +272,9 @@ vector<vector<string>> crossover(vector<vector<string>> parents, map<string, vec
 		rule_1.push_back(father_data.at(i));
 		rule_2.push_back(father_data.at(i));
 		rule_2.push_back(mother_data.at(i));
-		
 		rules.push_back(rule_1);
 		rules.push_back(rule_2);
 	}
-	if (DEBUG == 1) cout << "Rules:" << endl;
-	if (DEBUG == 1) print_array_of_paths(rules);
 
 	// FILLING CHILDREN EMPTY GENES 1
 	for (unsigned int i = 0; i < smaller; ++i){
@@ -314,13 +288,6 @@ vector<vector<string>> crossover(vector<vector<string>> parents, map<string, vec
 		}
 	}
 
-	if (DEBUG == 1) cout << "After filling sub gene 1:" << endl;
-	if (DEBUG == 1) print_path(parents.at(0));
-	if (DEBUG == 1) print_path(daughter);
-	if (DEBUG == 1) cout << endl;
-	if (DEBUG == 1) print_path(parents.at(1));
-	if (DEBUG == 1) print_path(son);
-
 	// FILLING CHILDREN EMPTY GENES 1
 	for (unsigned int i = bigger; i < daughter.size(); ++i){
 		for (unsigned int j = 0; j < rules.size(); ++j){
@@ -333,13 +300,6 @@ vector<vector<string>> crossover(vector<vector<string>> parents, map<string, vec
 		}
 	}
 
-	if (DEBUG == 1) cout << "After filling sub gene 2:" << endl;
-	if (DEBUG == 1) print_path(parents.at(0));
-	if (DEBUG == 1) print_path(daughter);
-	if (DEBUG == 1) cout << endl;
-	if (DEBUG == 1) print_path(parents.at(1));
-	if (DEBUG == 1) print_path(son);
-
 	// COLLECTING UNUSED GENES FROM MOMMY
 	vector<string> unused_mommy_genes;
 	vector<string> unused_daddy_genes;
@@ -349,10 +309,6 @@ vector<vector<string>> crossover(vector<vector<string>> parents, map<string, vec
 	for (unsigned int i = 0; i < son.size(); ++i){
 		if (son.at(i) == " ") unused_daddy_genes.push_back( (parents.at(1)).at(i) ); 
 	}
-
-	if (DEBUG == 1)cout << "Remaing genes:" << endl;
-	if (DEBUG == 1)print_path(unused_mommy_genes);
-	if (DEBUG == 1)print_path(unused_daddy_genes);
 
 	// FILLING REMAINING EMPTY GENES IN CHILDREN
 	unsigned int index = 0;
@@ -370,12 +326,7 @@ vector<vector<string>> crossover(vector<vector<string>> parents, map<string, vec
 		}
 	}
 
-	if (DEBUG == 1) cout << "After full crossing:" << endl;
-	if (DEBUG == 1) print_path(parents.at(0));
-	if (DEBUG == 1) print_path(daughter);
-	if (DEBUG == 1) cout << endl;
-	if (DEBUG == 1) print_path(parents.at(1));
-	if (DEBUG == 1) print_path(son);	
+	// FILLING THE RESULT
 	children.push_back(daughter);
 	children.push_back(son);
 	return children;
@@ -383,60 +334,49 @@ vector<vector<string>> crossover(vector<vector<string>> parents, map<string, vec
 
 vector<vector<string>> pick_two(vector<vector<string>> population)
 {
-	int counter = 0;		
-
+	// PICKS RANDOM TWO FROM POPULATION
 	vector<vector<string>> pair;
+	
 	while (pair.size() != 2){
-		clock_t begin_pt = clock();
+
+		// CHOOSING RANDOM FROM POPULATION
 		unsigned int random = get_random_num( population.size() );
-		clock_t end_pt = clock();
-		//cout << "PICK_TWO. Time spent on generating random number " << double(end_pt - begin_pt) / CLOCKS_PER_SEC << endl;
-		//cout << random << endl;
 		vector<string> choosen = population.at(random);
+
+		// IF THIS IS FIRST ONE - ASSIGN TO PAIR
 		if (pair.size() == 0){
-			//cout << " Pushing fist one." << endl;
 			pair.push_back(choosen);
 		}
+		// ELSE ASSIGN TO PAIR ONLY IF CHOSEN ONE IS DIFFERENT
 		else {
-			int different = 0;
-			vector<string> first = pair.at(0);
-			if ( first != choosen ) pair.push_back(choosen);
-			// for (unsigned int i = 0; i < choosen.size(); ++i){
-			// 	if (choosen.at(i) != first.at(i)){
-			// 		++different;
-			// 		break;
-			// 	}
-			// }
-			// if (different != 0){
-			// 	pair.push_back(choosen);
-			// }
+			if (pair.at(0) != choosen) pair.push_back(choosen);
 		}
-		++counter;
 	}
-	//cout << "PICK_TWO. Tried to generate pair for " << counter << " times.";
-
 	return pair;
 }
 
 int get_random_num(int mod)
 {
-	
-	return rand() % mod ;
+	return rand() % mod;
 }
 
-vector<vector<string>> pick_better_half(vector<vector<string>> population, 
-										map<string, vector<int> > mapa, 
-										unsigned int worst_possible_path_lenght)
+vector<vector<string>> pick_better_half(vector<vector<string>> population, map<string, vector<int> > mapa, unsigned int worst_possible_path_lenght)
 {
-	// GETTING BETTER HALF
+	// GETTING BETTER HALF OF POPULATION FOR POTENCIAL CROSSOVER
 	vector<vector<string>> better_half;
 	vector<vector<string>> local_population = population;
 
+	// PICKING BEST OF THE BEST ONE BY ONE
 	for (unsigned int i = 0; i < population.size()/2; ++i){
+
+		// PICK SINGLE BEST
 		vector<string> best_one = find_best_path(local_population, mapa, worst_possible_path_lenght);
 		better_half.push_back(best_one);
+
+		// REMOVING CURRENT PICK FROM POPULATION
 		for (unsigned int j = 0; j < local_population.size(); ++j){
 			if (best_one == local_population.at(j)){
+				// REMOVAL
 				local_population.erase(
 					std::remove(local_population.begin(), local_population.end(), best_one), 
 					local_population.end()
@@ -447,16 +387,17 @@ vector<vector<string>> pick_better_half(vector<vector<string>> population,
 	return better_half;
 }
 
-vector<string> find_best_path(vector<vector<string>> population, 
-								map<string, vector<int> > mapa, 
-								unsigned int worst_possible_path_lenght)
+vector<string> find_best_path(vector<vector<string>> population, map<string, vector<int> > mapa, unsigned int worst_possible_path_lenght)
 {
 	// FINDS BEST SOLUTION IN POPULATION
 	vector <string> best_path;
 	double best_path_length = worst_possible_path_lenght;
+
+	// ITERATING OVER ALL POPULATION
 	for (unsigned int i = 0; i < population.size(); ++i){
 		double length = get_path_length(population.at(i), mapa);
 		if (length < best_path_length){
+			// BEST ONE FOUND
 			best_path_length = length;
 			best_path = population.at(i);
 		}
@@ -471,11 +412,10 @@ bool contains_cycles(vector<string> path)
 		int check = 0;
 		for (unsigned int j = 0; j < path.size(); ++j){
 			if (path.at(i) == path.at(j)){
+				// CYCLE FOUND
 				++check;
+				if (check > 1) return true;
 			}
-		}
-		if (check > 1){
-			return true;
 		}
 	}
 	return false;
@@ -498,22 +438,13 @@ double get_distance(string s1, string s2, map<string, vector<int> > mapa)
 	int y1 = mapa[s1].at(1);
 	int x2 = mapa[s2].at(0);
 	int y2 = mapa[s2].at(1);
-	//cout << "Calculating for " << x1 << "," << y1 << "," << x2 << "," << y2 << endl;
 	return sqrt(pow((x1 - x2), 2) + pow((y1 - y2), 2));
-}
-
-void print_vector(vector<string> vec)
-{
-	// PRINTS VECTOR
-	for (unsigned int i = 0; i < vec.size(); ++i){
-		cout << "[" << i << "] = " << vec.at(i) << endl;
-	}
 }
 
 void print_path(vector<string> path){
 	// PRINTS PATH
 	for (unsigned int i = 0; i < path.size(); ++i){
-		cout << "[" << path.at(i) << "] ";
+		cout << path.at(i) << " ";
 	}
 	cout << endl;
 }
@@ -521,9 +452,9 @@ void print_path(vector<string> path){
 void print_path_and_values(vector<string> path, map<string, vector<int> > mapa){
 	// PRINTS PATH
 	for (unsigned int i = 0; i < path.size(); ++i){
-		cout << "[" << path.at(i) << "] ";
+		cout << path.at(i) << " ";
 	}
-	//cout << "\t" << get_path_length(path, mapa);
+	cout << "\t" << get_path_length(path, mapa);
 	cout << endl;
 }
 
@@ -556,16 +487,6 @@ vector<string> shuffle(vector<string> nodes){
 	return nodes;
 }
 
-bool compare_two(vector<string> v1, vector<string> v2){
-	// COMPARES TWO PATHS
-	for (unsigned int i = 0; i < v1.size(); ++i){
-		if (v1.at(i) != v2.at(i)){
-			return false; // DIFFERENT
-		}
-	}
-	return true; // THE SAME
-}
-
 vector< vector<string> > create_initial_population(map<string, vector<int> > mapa, unsigned int population_size){
 	// CREATES INITIAL POPULATION
 	vector<vector<string>> population;
@@ -584,7 +505,7 @@ vector< vector<string> > create_initial_population(map<string, vector<int> > map
 			// ORDERS OF GENES IN POPULATION
 			unsigned int check = 0;
 			for (unsigned int i = 0; i < population.size(); ++i){
-				if (compare_two(being, population.at(i))){
+				if (being == population.at(i)){
 					++check;
 				}
 			}
